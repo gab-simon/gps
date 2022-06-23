7865a62f26eb795cb91de91774050fd61a93c65a
simon <simon@pop-os.localdomain>
2022-06-23 16:08:35 -0300

    cat especific value

--- encontraLog.c
+++ encontraLog.c
@@ -1,32 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define LINESIZE 1024
 
 int main()
 {
   FILE *arq;
-  int i;
   char line[LINESIZE + 1];
-  // abre o arquivo em escrita
-  arq = fopen("4252839552.log", "r");
+  char *separator;
+  double valor;
+  char *content;
+  char *eptr;
+
+  separator = malloc(LINESIZE);
+  eptr = malloc(LINESIZE);
+
+  // abre o arquivo
+  arq = fopen("4252839552.log", "rw");
   if (!arq)
   {
     perror("Erro ao abrir/criar arquivo");
     exit(1);
   }
 
-  // escreve os caracteres ascii
   // TODO: strtok
-  i = 1;
   fgets(line, LINESIZE, arq); // tenta ler uma linha
   while (!feof(arq))          // testa depois de tentar ler!
   {
-    printf("%d: %s", i, line);
+    separator = strtok(line, ": ");
+    printf("%s\n", separator);
+
+    content = strtok(NULL, " ");
+    if (strcmp(separator, "altitude") == 0)
+    {
+      valor = strtod(content, &eptr);
+      printf("valor: %f\n", valor);
+    }
+    // printf("%s \n", separator);
+
     fgets(line, LINESIZE, arq); // tenta ler a próxima linha
-    i++;
   }
 
   // fecha o arquivo
+  free(separator);
   fclose(arq);
 }
