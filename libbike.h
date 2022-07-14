1935ef2bbe5cd5dccfdc46390cdd9012482359db
simon <simon@pop-os.localdomain>
2022-07-14 16:22:03 -0300

    Fix Struct

--- libbike.h
+++ libbike.h
@@ -4,31 +4,71 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct bike {
-    double cadence;
-    double speed;
-    double distance;
-    double heart_rate;
-    double enhanced_altitude;
-};
-typedef struct bike;
-struct nodo_l {
-    struct bike;
-    struct nodo_l* prox;
-};
-typedef struct nodo_l nodo_l_t;
+typedef struct Date
+{
+    int day;
+    int mounth;
+    int year;
 
-struct lista {
-    nodo_l_t* ini;
+} date_t;
+
+typedef struct BikeData
+{
+    date_t Date;
+    char *gear;
+    double distance;
+
+    double speedTotal;
+    double speedMed;
+    double speedMax;
+
+    double heartRateTotal;
+    double heartRateMed;
+    double heartRateMax;
+    // int hrValid; da pra ser uma variavel local
+
+    double cadenceTotal;
+    double cadenceMed;
+    // int cadenceVal; da pra ser uma variavel local
+
+    double altitude;
+    double clunth_altitude;
+
+    int quantData;
+} BikeData_t;
+
+typedef struct Bike
+{
+    BikeData_t *bike;
+    struct bike *nextBike;
+    int id;
+} Bike_t;
+
+typedef struct BikeRack
+{
+
+    struct BikeRack *nextRack;
+    struct BikeRack *backRack;
+    Bike_t *element;
+    Bike_t *lastElement;
+
+    int quant;
+    char *gear;
+
+} BikeRack_t;
+
+struct lista
+{
+    nodo_l_t *ini;
     int tamanho;
     char nome_bike[100];
 };
 typedef struct lista lista_t;
 
-lista_t* lista_cria ();
-lista_t* lista_destroi (lista_t* l);
-int lista_insere_inicio (lista_t* l, double distance);
-void lista_imprime (lista_t* l);
+lista_t *lista_cria();
+lista_t *lista_destroi(lista_t *l);
+int lista_insere_inicio(lista_t *l, double distance);
+void lista_imprime(lista_t *l);
 int open_log(char *file, char *namepath);
 
 #endif
